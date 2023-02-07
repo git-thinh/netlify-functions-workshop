573f7dc0eed42dccfae541b6cc32b71319affeda
Mr Thinh <33323583+git-thinh@users.noreply.github.com>
2023-02-07 21:58:45 +0700

    34534

--- lessons-code-complete/core-concepts/6-using-a-database/functions/mongo/mongo.js
+++ lessons-code-complete/core-concepts/6-using-a-database/functions/mongo/mongo.js
@@ -1,44 +1,48 @@
-const { MongoClient } = require('mongodb')
-const { MONGODB_URI } = process.env
+const {
+	MongoClient
+} = require('mongodb')
+const {
+	MONGODB_URI
+} = process.env
 
 let cachedDb = null
 
-function connectToDatabase (uri) {
-  console.log('=> connect to database')
+function connectToDatabase(uri) {
+	console.log('=> connect to database')
 
-  if (cachedDb) {
-    console.log('=> using cached database instance')
-    return Promise.resolve(cachedDb)
-  }
+	if (cachedDb) {
+		console.log('=> using cached database instance')
+		return Promise.resolve(cachedDb)
+	}
 
-  return MongoClient.connect(uri).then((db) => {
-    cachedDb = db
-    return cachedDb
-  })
+	return MongoClient.connect(uri).then((db) => {
+		cachedDb = db
+		return cachedDb
+	})
 }
 
 exports.handler = async (event, context) => {
-  context.callbackWaitsForEmptyEventLoop = false
+	context.callbackWaitsForEmptyEventLoop = false
 
-  let result
-  try {
-    const db = await connectToDatabase(MONGODB_URI)
-    result = await db.collection('items').find({}).toArray()
-    console.log('result', result)
-  } catch (error) {
-    console.log('error', error)
-    return {
-      statusCode: error.statusCode || 500,
-      body: JSON.stringify({
-        message: error.message
-      })
-    }
-  }
+	let result
+	try {
+		const db = await connectToDatabase(MONGODB_URI)
+		result = await db.collection('items').find({}).toArray()
+		console.log('result', result)
+	} catch (error) {
+		console.log('error', error)
+		return {
+			statusCode: error.statusCode || 500,
+			body: JSON.stringify({
+				message: error.message
+			})
+		}
+	}
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      data: result
-    })
-  }
+	return {
+		statusCode: 200,
+		body: JSON.stringify({
+			data: result
+		})
+	}
 }
